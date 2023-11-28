745e1ce4ffab6ee289eb1bf1629c2d2445cdfe83
iceiceninja <joshdbonham@gmail.com>
2023-11-28 01:37:15 -0600

    initial commit

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form action="getData.php" method="get">
+            EID:    <input type="text" name="EID"><br>
+            Password:   <input type="text" name="Password"><br>
+            <input type="submit" value="Submit">
+        </form>
+        
+    </body>
+</html>
+<!-- 'OR 1=1;# -->
