63cdc75d97476e4d72348491eaab6f103553fe75
rayveng1 <rayvengsell@gmail.com>
2023-11-30 16:53:45 -0600

    Update home.php

--- home.php
+++ home.php
@@ -30,16 +30,20 @@
             animation: wiggle 1s ease-in-out infinite;
         }
 </style>
-<body>
-    <h1>Welcome Bookworm!</h1>
-    <a href="book_manager.php">Book Manager</a>
-    <br>
-    <p style="box-shadow: 1px 1px 6px black; border: 1px solid black; padding: 5px;">
-Welcome to BookWorm, your digital gateway to a world of books! 📚 Dive into an immersive interface connecting you with a 
-vast database of literary treasures. Explore, discover, and manage your reading journey effortlessly. 
-From adding new gems to updating your collection, BookWorm is your companion in the realm of literature. 
-Let the adventure begin as you turn the virtual pages of your personalized book haven with BookWorm! 🌐📖 
-#BookWorm #ReadingAdventure</p>
-    <img src="bookWorm.png" alt="Book Worm" class="w-32 h-32 mt-4 animate-wiggle">
+<body class="flex flex-col items-center justify-between min-h-screen bg-gray-100">
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold mb-4">Welcome Bookworm!</h1>
+        <a href="book_manager.php" class="text-blue-500 hover:text-blue-700">Book Manager</a>
+        <br>
+        <p class="box-border p-5 shadow-md border border-black">
+            Welcome to BookWorm, your digital gateway to a world of books! 📚 Dive into an immersive interface connecting you with a 
+            vast database of literary treasures. Explore, discover, and manage your reading journey effortlessly. 
+            From adding new gems to updating your collection, BookWorm is your companion in the realm of literature. 
+            Let the adventure begin as you turn the virtual pages of your personalized book haven with BookWorm! 🌐📖 
+            #BookWorm #ReadingAdventure
+        </p>
+        <img src="bookWorm.png" alt="Book Worm" class="w-32 h-32 mt-4 animate-wiggle">
+    </div>
+    <img src="grass.png" alt="grass" style="width: 100%; position: fixed; bottom: 0; left: 0;">
 </body>
 </html>
